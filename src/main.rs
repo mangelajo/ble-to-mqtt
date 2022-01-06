0df9a98228620cf8835476338f15c149a2907da6
Miguel Angel Ajo <miguelangel@ajo.es>
2022-01-06 19:16:37 +0000

    Update

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,24 @@ mod config;
 use btleplug::api::{Central, CentralEvent, Manager as _, ScanFilter};
 use btleplug::platform::{Adapter, Manager};
 use byteorder::{ByteOrder, LittleEndian};
-use futures::stream::StreamExt;
-use std::error::Error;
 use clap::Parser;
+use futures::stream::StreamExt;
+use paho_mqtt as mqtt;
+use serde_json::json;
 use std::collections::HashMap;
+use std::error::Error;
+use std::{env, process};
+extern crate env_logger;
+extern crate pretty_env_logger;
+#[macro_use]
+extern crate log;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[clap(about, version, author)]
 struct Args {
     // Config file to parse
-    #[clap(short, long, default_value= "/etc/ble-receiver.yaml")]
+    #[clap(short, long, default_value = "/etc/ble-to-mqtt.yaml")]
     config: String,
 }
 
@@ -24,13 +31,17 @@ async fn get_central(manager: &Manager) -> Adapter {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    match env::var("RUST_LOG") {
+        Err(_e) => env::set_var("RUST_LOG", "info"),
+        _ => {}
+    }
     pretty_env_logger::init();
 
     let args = Args::parse();
 
     let cfg = config::read(args.config)?;
 
-    println!("cfg {:?}", cfg);
+    let cli = mqtt_connect(&cfg);
 
     let mut sensors = HashMap::new();
 
@@ -51,31 +62,28 @@ async fn main() -> Result<(), Box<dyn Error>> {
     while let Some(event) = events.next().await {
         match event {
             CentralEvent::DeviceDiscovered(id) => {
-                println!("DeviceDiscovered: {:?}", id);
+                debug!("DeviceDiscovered: {:?}", id);
             }
-           
+
             CentralEvent::ManufacturerDataAdvertisement {
                 id,
                 manufacturer_data,
             } => {
                 let peripheralid_mac = format!("{:?}", id).to_uppercase();
-                let mac = &peripheralid_mac[13..=13+16];
+                let mac = &peripheralid_mac[13..=13 + 16];
 
                 if let Some(data) = manufacturer_data.get(&0x8801_u16) {
                     let temp = LittleEndian::read_i16(&data[4..=5]) as f32 / 100.0;
                     let hum = LittleEndian::read_i16(&data[6..=7]) as f32 / 100.0;
                     let batt = &data[8];
-                    println!("{} -> Temp: {}C RH:{}% batt: {}%", mac, temp, hum, batt);
-
-                    let json = format!("{\"temperature\":\"{}\", \"RH\":\"{}\", battery:\"{}\"}",
-                        temp, hum, batt);
+                    info!("{} -> Temp: {}C RH:{}% batt: {}%", mac, temp, hum, batt);
 
                     match sensors.get(&*mac) {
-                        Some(sensor) => println!("Sensor: {:?}", sensor),
-                        None => println!("Received temp for sensor {} not in config", mac),
+                        Some(sensor) => mqtt_publish_sensor(&cli, sensor, temp, hum, *batt),
+                        None => warn!("Received temp for sensor {} not in config", mac),
                     }
                 } else {
-                    println!(
+                    debug!(
                         "ManufacturerDataAdvertisement: {:?}, {:?}",
                         id, manufacturer_data
                     );
@@ -86,4 +94,50 @@ async fn main() -> Result<(), Box<dyn Error>> {
         }
     }
     Ok(())
+}
+
+fn mqtt_publish_sensor(
+    cli: &mqtt::client::Client,
+    sensor: &config::Sensor,
+    temp: f32,
+    hum: f32,
+    batt: u8,
+) {
+    let json_data = json!({"temperature": temp, "RH": hum, "battery": batt});
+
+    let msg_charge = mqtt::MessageBuilder::new()
+        .topic(&sensor.mqtt_publish)
+        .payload(json_data.to_string())
+        .qos(1)
+        .retained(true)
+        .finalize();
+
+    if let Err(e) = cli.publish(msg_charge) {
+        error!("Error sending message: {:?}", e);
+        process::exit(1);
+    }
+}
+
+fn mqtt_connect(config: &config::Config) -> mqtt::client::Client {
+    let create_opts = mqtt::CreateOptionsBuilder::new()
+        .server_uri(&config.mqtt_config.endpoint)
+        .client_id(&config.mqtt_config.client_id)
+        .max_buffered_messages(100)
+        .finalize();
+
+    let cli = mqtt::Client::new(create_opts).unwrap_or_else(|e| {
+        error!("Error creating the client: {:?}", e);
+        process::exit(1);
+    });
+
+    let conn_opts = mqtt::ConnectOptionsBuilder::new()
+        .user_name(&config.mqtt_config.username)
+        .password(&config.mqtt_config.password)
+        .finalize();
+
+    cli.connect(conn_opts).unwrap();
+
+    info!("Connected to {}", config.mqtt_config.endpoint);
+
+    return cli;
 }
